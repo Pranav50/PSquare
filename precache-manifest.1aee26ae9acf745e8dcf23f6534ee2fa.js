25b9f5a9ff52fb268ada86e99420a84a93686b86
Pranav5000 <pranavpw815@gmail.com>
2019-12-26 14:20:18 +0530

    Updates

---
+++ precache-manifest.1aee26ae9acf745e8dcf23f6534ee2fa.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7dab29d02a2403b00e2be17fde32894",
+    "url": "/PSquare/index.html"
+  },
+  {
+    "revision": "734391b7cddc1f61bc32",
+    "url": "/PSquare/static/css/2.83f7f6c4.chunk.css"
+  },
+  {
+    "revision": "5d551a893ead476edb55",
+    "url": "/PSquare/static/css/main.fc09746f.chunk.css"
+  },
+  {
+    "revision": "734391b7cddc1f61bc32",
+    "url": "/PSquare/static/js/2.e0fc9fd7.chunk.js"
+  },
+  {
+    "revision": "074562c3afe0c9e91143c7365f6ec3b8",
+    "url": "/PSquare/static/js/2.e0fc9fd7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d551a893ead476edb55",
+    "url": "/PSquare/static/js/main.8792e41a.chunk.js"
+  },
+  {
+    "revision": "1490b1d49e12dc3deb8b",
+    "url": "/PSquare/static/js/runtime-main.1d1fd51e.js"
+  },
+  {
+    "revision": "ba2818813a26df3e5a172b87f9c8e40b",
+    "url": "/PSquare/static/media/Pranav_new2.ba281881.png"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/PSquare/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/PSquare/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/PSquare/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/PSquare/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/PSquare/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/PSquare/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/PSquare/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/PSquare/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/PSquare/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/PSquare/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/PSquare/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/PSquare/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/PSquare/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/PSquare/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/PSquare/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/PSquare/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/PSquare/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/PSquare/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/PSquare/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/PSquare/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/PSquare/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/PSquare/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "4080a2de7240a722fbd56b6a24ec4706",
+    "url": "/PSquare/static/media/beach-resort.4080a2de.jpg"
+  },
+  {
+    "revision": "1511cd9196d82ce02c1062dbdb4d6fad",
+    "url": "/PSquare/static/media/bullet3.1511cd91.png"
+  },
+  {
+    "revision": "9cdbd41048a19398aaeb51a668be4f27",
+    "url": "/PSquare/static/media/bullet4.9cdbd410.png"
+  },
+  {
+    "revision": "771f05020ca2d7a0c88bcf84f61175ee",
+    "url": "/PSquare/static/media/disc3.771f0502.png"
+  },
+  {
+    "revision": "a65fa6d3edc1c85c2c2841d12ad419e7",
+    "url": "/PSquare/static/media/drop.a65fa6d3.png"
+  },
+  {
+    "revision": "6d57dd2c11d04e584a4d183a906c7bd5",
+    "url": "/PSquare/static/media/ecom-angular.6d57dd2c.jpg"
+  },
+  {
+    "revision": "38975343b586296e9b73e6b56cc3ec5d",
+    "url": "/PSquare/static/media/fa-brands-400.38975343.svg"
+  },
+  {
+    "revision": "8e49b728413079dfd9ee45d0c58d54e4",
+    "url": "/PSquare/static/media/fa-brands-400.8e49b728.eot"
+  },
+  {
+    "revision": "9f018d1025561e96439f7c0e9026301a",
+    "url": "/PSquare/static/media/fa-brands-400.9f018d10.woff"
+  },
+  {
+    "revision": "9f4ce3dc689981a1b87faab0f5484f9e",
+    "url": "/PSquare/static/media/fa-brands-400.9f4ce3dc.woff2"
+  },
+  {
+    "revision": "b7d071b9c3c197bff4af902070622423",
+    "url": "/PSquare/static/media/fa-brands-400.b7d071b9.ttf"
+  },
+  {
+    "revision": "7980a6361c25b4665dbbe92d4488783c",
+    "url": "/PSquare/static/media/fa-regular-400.7980a636.woff2"
+  },
+  {
+    "revision": "7aaf5675efd6339e9aba53ecbe5f1e36",
+    "url": "/PSquare/static/media/fa-regular-400.7aaf5675.woff"
+  },
+  {
+    "revision": "859001f6ae8eb0bb3878aaa971b50fc6",
+    "url": "/PSquare/static/media/fa-regular-400.859001f6.eot"
+  },
+  {
+    "revision": "da8a235bb207c74eea21507f3a86a53b",
+    "url": "/PSquare/static/media/fa-regular-400.da8a235b.svg"
+  },
+  {
+    "revision": "f33342516f7cbe46f1d6b68f9e7bbeda",
+    "url": "/PSquare/static/media/fa-regular-400.f3334251.ttf"
+  },
+  {
+    "revision": "0be94a07755ba9b88f2ebcac0f23a3da",
+    "url": "/PSquare/static/media/fa-solid-900.0be94a07.woff"
+  },
+  {
+    "revision": "64b3e814a66c2719b15abf8f7998bd73",
+    "url": "/PSquare/static/media/fa-solid-900.64b3e814.woff2"
+  },
+  {
+    "revision": "7726a281c1d436eb038f78c6e9048c96",
+    "url": "/PSquare/static/media/fa-solid-900.7726a281.svg"
+  },
+  {
+    "revision": "e2675a616b68f446fa6284c111554c7f",
+    "url": "/PSquare/static/media/fa-solid-900.e2675a61.eot"
+  },
+  {
+    "revision": "f14c3b2ff7c821a4c838debbffd6ad2d",
+    "url": "/PSquare/static/media/fa-solid-900.f14c3b2f.ttf"
+  },
+  {
+    "revision": "62fb2fb89d6228d19d57ae7f7526bdf9",
+    "url": "/PSquare/static/media/image-finder.62fb2fb8.jpg"
+  },
+  {
+    "revision": "b11e890196c0c65fbb85382207ca5845",
+    "url": "/PSquare/static/media/lyrics-finder.b11e8901.jpg"
+  },
+  {
+    "revision": "3208674732940a092af2950c22721fa5",
+    "url": "/PSquare/static/media/movie-finder.32086747.jpg"
+  },
+  {
+    "revision": "b5d63737b5b27a678d811eb09bc6d363",
+    "url": "/PSquare/static/media/mshop.b5d63737.jpg"
+  },
+  {
+    "revision": "ae0a545577465185cf07a77f14f4bc74",
+    "url": "/PSquare/static/media/slack-clone.ae0a5455.jpg"
+  },
+  {
+    "revision": "9e0776f78cd63811093d777dca8faa95",
+    "url": "/PSquare/static/media/todo-list-firebase.9e0776f7.jpg"
+  },
+  {
+    "revision": "54bfee0b94d97f558c30bf5cb2537f8f",
+    "url": "/PSquare/static/media/todo-list.54bfee0b.jpg"
+  },
+  {
+    "revision": "85cf70f421cab561a0f37416449b6142",
+    "url": "/PSquare/static/media/todo-mern.85cf70f4.jpg"
+  },
+  {
+    "revision": "1f7a99995fc80c50a0385e7b2147f18f",
+    "url": "/PSquare/static/media/weather.1f7a9999.jpg"
+  },
+  {
+    "revision": "d2bac365c41c5f755595ec35d5f5c75e",
+    "url": "/PSquare/static/media/youtube-clone.d2bac365.jpg"
+  }
+]);
